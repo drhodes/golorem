9228f57cecae8a4671c5e4d72000b4a776e2f624
drhodes <physci@gmail.com>
2012-01-19 17:23:26 -0500

    updated readme

--- lorem_test.go
+++ lorem_test.go
@@ -1,3 +1,7 @@
+// Copyright 2010 Derek A. Rhodes.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package lorem
 
 import "testing"
